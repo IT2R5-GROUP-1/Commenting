fdbfaa39f6b0febf99d422c3692008139b7329c7
kintstack <kintjohn12@gmail.com>
2025-05-10 06:29:38 -0800

    Initial Commit

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -10,54 +10,55 @@ use Illuminate\Support\Facades\Validator;
 class CommentController extends Controller
 {
     // Add comment to a post
-    public function addComment($postId, Request $request)
-    {
-        $post = Post::findOrFail($postId);
+public function addComment($postId, Request $request)
+{
+    $post = Post::findOrFail($postId);
 
-        // Manual validation using Validator
-        $validator = Validator::make($request->all(), [
-            'text' => 'required|string',
-        ]);
+    $validator = Validator::make($request->all(), [
+        'text' => 'required|string',
+        'username' => 'nullable|string|max:255', // ← Add this line
+    ]);
 
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 422);
-        }
-
-        $comment = new Comment();
-        $comment->username = 'User_' . substr(md5(rand()), 0, 9); // Example username
-        $comment->text = $request->text;
-        $comment->post_id = $post->id;
-        $comments = $post->comments;
-
-        $comment->save();
-
-        return response()->json($comment, 201);
+    if ($validator->fails()) {
+        return response()->json(['errors' => $validator->errors()], 422);
     }
+
+    $comment = new Comment();
+    $comment->username = $request->username ?? 'User_' . substr(md5(rand()), 0, 9); // ← Use passed username or fallback
+    $comment->text = $request->text;
+    $comment->post_id = $post->id;
+
+    $comment->save();
+
+    return response()->json($comment, 201);
+}
+
 
     // Reply to a comment
-    public function replyToComment($commentId, Request $request)
-    {
-        $comment = Comment::findOrFail($commentId);
+public function replyToComment($commentId, Request $request)
+{
+    $comment = Comment::findOrFail($commentId);
 
-        // Manual validation using Validator
-        $validator = Validator::make($request->all(), [
-            'text' => 'required|string',
-        ]);
+    $validator = Validator::make($request->all(), [
+        'text' => 'required|string',
+        'username' => 'nullable|string|max:255', // ← Add this too
+    ]);
 
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 422);
-        }
-
-        $reply = new Comment();
-        $reply->username = 'User_' . substr(md5(rand()), 0, 9); // Example username
-        $reply->text = $request->text;
-        $reply->post_id = $comment->post_id;
-        $reply->parent_id = $comment->id;
-
-        $reply->save();
-
-        return response()->json($reply, 201);
+    if ($validator->fails()) {
+        return response()->json(['errors' => $validator->errors()], 422);
     }
+
+    $reply = new Comment();
+    $reply->username = $request->username ?? 'User_' . substr(md5(rand()), 0, 9); // ← Use frontend username
+    $reply->text = $request->text;
+    $reply->post_id = $comment->post_id;
+    $reply->parent_id = $comment->id;
+
+    $reply->save();
+
+    return response()->json($reply, 201);
+}
+
 
     // Get all comments for a post
 public function getComments($postId)
